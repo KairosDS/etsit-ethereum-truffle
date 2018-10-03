6b070c6bda18c29e0273ef164685ee3f4034eab1
paulaot14 <paula.otero.catedra@kairosds.com>
2018-10-03 10:45:10 +0200

    tests added

--- test/election.js
+++ test/election.js
@@ -4,50 +4,54 @@ var Election = artifacts.require("./Election.sol");
 contract("Election", function(accounts) {
   var electionInstance;
 
-  it("initializes with two candidates", function() {
+  it("se inicializa con 3 equipos", function() {
     return Election.deployed().then(function(instance) {
       return instance.candidatesCount();
     }).then(function(count) {
-      assert.equal(count, 2);
+      assert.equal(count, 3);
     });
   });
 
-  it("it initializes the candidates with the correct values", function() {
+  it("Se inicializan los equipos con los parametros correctos", function() {
     return Election.deployed().then(function(instance) {
       electionInstance = instance;
       return electionInstance.candidates(1);
     }).then(function(candidate) {
-      assert.equal(candidate[0], 1, "contains the correct id");
-      assert.equal(candidate[1], "Candidate 1", "contains the correct name");
-      assert.equal(candidate[2], 0, "contains the correct votes count");
+      assert.equal(candidate[0], 1, "contiene el id correcto");
+      assert.equal(candidate[1], "Equipo 1", "contiene el nombre correcto");
+      assert.equal(candidate[2], 0, "contiene el numero correcto de votos");
       return electionInstance.candidates(2);
     }).then(function(candidate) {
-      assert.equal(candidate[0], 2, "contains the correct id");
-      assert.equal(candidate[1], "Candidate 2", "contains the correct name");
-      assert.equal(candidate[2], 0, "contains the correct votes count");
+      assert.equal(candidate[0], 2, "contiene el id correcto");
+      assert.equal(candidate[1], "Equipo 2", "contiene el nombre correcto");
+      assert.equal(candidate[2], 0, "contiene el numero correcto de votos");
+      return electionInstance.candidates(3);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 3, "contiene el id correcto");
+      assert.equal(candidate[1], "Equipo 3", "contiene el nombre correcto");
+      assert.equal(candidate[2], 0, "contiene el numero correcto de votos");
     });
   });
 
-  it("allows a voter to cast a vote", function() {
+  it("permite al votante emitir el voto", function() {
     return Election.deployed().then(function(instance) {
       electionInstance = instance;
       candidateId = 1;
       return electionInstance.vote(candidateId, { from: accounts[0] });
     }).then(function(receipt) {
-      assert.equal(receipt.logs.length, 1, "an event was triggered");
-      assert.equal(receipt.logs[0].event, "votedEvent", "the event type is correct");
-      assert.equal(receipt.logs[0].args._candidateId.toNumber(), candidateId, "the candidate id is correct");
+      assert.equal(receipt.logs.length, 1, "un evento ha sido capturado");
+      assert.equal(receipt.logs[0].event, "votedEvent", "es el evento correcto");
       return electionInstance.voters(accounts[0]);
     }).then(function(voted) {
-      assert(voted, "the voter was marked as voted");
+      assert(voted, "el votante ha sido marcado como votado");
       return electionInstance.candidates(candidateId);
     }).then(function(candidate) {
       var voteCount = candidate[2];
-      assert.equal(voteCount, 1, "increments the candidate's vote count");
+      assert.equal(voteCount, 1, "incrementa el recuento de votos del candidato");
     })
   });
 
-  it("throws an exception for invalid candiates", function() {
+  it("lanza una excepcion para candidatos invalidos", function() {
     return Election.deployed().then(function(instance) {
       electionInstance = instance;
       return electionInstance.vote(99, { from: accounts[1] })
@@ -56,15 +60,19 @@ contract("Election", function(accounts) {
       return electionInstance.candidates(1);
     }).then(function(candidate1) {
       var voteCount = candidate1[2];
-      assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+      assert.equal(voteCount, 1, "el candidato 1 no ha recibido ningun voto");
       return electionInstance.candidates(2);
     }).then(function(candidate2) {
       var voteCount = candidate2[2];
-      assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
+      assert.equal(voteCount, 0, "el candidato 2 no ha recibido ningun voto");
+      return electionInstance.candidates(3);
+    }).then(function(candidate3) {
+      var voteCount = candidate3[2];
+      assert.equal(voteCount, 0, "el candidato 3 no ha recibido ningun voto");
     });
   });
 
-  it("throws an exception for double voting", function() {
+  it("lanza una excepción para votaciones dobles", function() {
     return Election.deployed().then(function(instance) {
       electionInstance = instance;
       candidateId = 2;
@@ -72,7 +80,7 @@ contract("Election", function(accounts) {
       return electionInstance.candidates(candidateId);
     }).then(function(candidate) {
       var voteCount = candidate[2];
-      assert.equal(voteCount, 1, "accepts first vote");
+      assert.equal(voteCount, 1, "acepta el primer voto");
       // Try to vote again
       return electionInstance.vote(candidateId, { from: accounts[1] });
     }).then(assert.fail).catch(function(error) {
@@ -80,11 +88,15 @@ contract("Election", function(accounts) {
       return electionInstance.candidates(1);
     }).then(function(candidate1) {
       var voteCount = candidate1[2];
-      assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+      assert.equal(voteCount, 1, "el candidato 1 no ha recibido ningun voto");
       return electionInstance.candidates(2);
     }).then(function(candidate2) {
       var voteCount = candidate2[2];
-      assert.equal(voteCount, 1, "candidate 2 did not receive any votes");
+      assert.equal(voteCount, 1, "el candidato 2 no ha recibido ningun voto");
+      return electionInstance.candidates(3);
+    }).then(function(candidate2) {
+      var voteCount = candidate2[2];
+      assert.equal(voteCount, 0, "el candidato 3 no ha recibido ningun voto");
     });
   });
 });
